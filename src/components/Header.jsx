1994e0ebfb6ff09bbe875e35c15c6add61ca3f00
F1bona4iPWNZ <60856881+F1bona4iPWNZ@users.noreply.github.com>
2021-02-20 17:16:52 +0200

    1st

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Header = () => {
+    return  <header className='header'> 
+    <img src='http://www.dejurka.ru/wp-content/uploads/2010/06/nexcitelogodesign.jpg'>
+    </img>
+    </header>
+}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Profile = () => {
+    return  <div className="content">
+    <div>ava</div>
+    <div>
+      my posts
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+  </div>
+}
+export default Profile
